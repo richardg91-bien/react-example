3c60d691b79d5522c1846ad25ffee910f990b4ec
richardg91_bien <richardg91@gmail.com>
2020-10-16 17:01:10 -0300

    Updates

---
+++ precache-manifest.2a784c62b5311240b52c05f064230962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6d3c31f9a11364cf9a190364b2d5cc",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "d750df4cee3d636f6583",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6bb2986e62d0184e4b5",
+    "url": "/react-example/static/js/2.d318f35f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-example/static/js/2.d318f35f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d750df4cee3d636f6583",
+    "url": "/react-example/static/js/main.b609eacd.chunk.js"
+  },
+  {
+    "revision": "591a35e8bba386688ec8",
+    "url": "/react-example/static/js/runtime-main.c33dc506.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
